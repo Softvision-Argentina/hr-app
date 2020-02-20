1db2c22f973e9ee7ccf7a4dd1eedf027cd1abcee
tom.manrey <tom.manrey@gmail.com>
2020-02-20 11:04:33 -0300

    moved the appHasRole directive to correct folder

--- 4.Clients/WebApi/ClientApp/src/app/app.module.ts
+++ 4.Clients/WebApi/ClientApp/src/app/app.module.ts
@@ -128,7 +128,7 @@ import { PostulantsComponent } from './postulants/postulants.component';
 import { PostulantsService } from './services/postulants.service';
 import { DeclineReasonComponent } from './decline-reasons/decline-reasons.component';
 import { DeclineReasonService } from './services/decline-reason.service';
-import { HasRoleDirective } from 'src/directives/appHasRole.directive';
+import { HasRoleDirective } from 'src/app/directives/appHasRole.directive';
 
 
 
