af926d635de94737d4d9a37c8065b35d0104bb59
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2020-03-06 10:30:29 -0300

    add user

--- 2.Infrastructure/Domain.Services.Repositories.EF/SeedMigrator.cs
+++ 2.Infrastructure/Domain.Services.Repositories.EF/SeedMigrator.cs
@@ -621,6 +621,7 @@ namespace Domain.Services.Repositories.EF
             var user20 = new User { FirstName = "Tomas", LastName = "Guagnini", Username = "tomas.guagnini@softvision.com", Role = Roles.Admin, Token = "", Password = "03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4" };
             var user21 = new User { FirstName = "Ignacio", LastName = "Casciaro", Username = "ignacio.casciaro@softvision.com", Role = Roles.Admin, Token = "", Password = "03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4" };
             var user22 = new User { FirstName = "Tomas", LastName = "Reisenman", Username = "tomas.reisenman@softvision.com", Role = Roles.Admin, Token = "", Password = "03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4" };
+            var user23 = new User { FirstName = "Gustavo", LastName = "Petruzzi", Username = "gustavo.petruzzi@softvision.com", Role = Roles.Admin, Token = "", Password = "03AC674216F3E15C761EE1A5E255F067953623C8B388B4459E13F978D7C846F4" };
 
 
             context.Users.Add(user2);
@@ -644,6 +645,7 @@ namespace Domain.Services.Repositories.EF
             context.Users.Add(user20);
             context.Users.Add(user21);
             context.Users.Add(user22);
+            context.Users.Add(user23);
             
             context.SaveChanges();
             #endregion
@@ -700,7 +702,8 @@ namespace Domain.Services.Repositories.EF
             var preference19 = new Preference { CasualtiesDashboard = true, ProcessesDashboard = true, CompletedDashboard = true, ProjectionDashboard = true, TimeToFill1Dashboard = true, TimeToFIll2Dashboard = true, SkillsDashboard = true, ProgressDashboard = true, User = user20 };
             var preference20 = new Preference { CasualtiesDashboard = true, ProcessesDashboard = true, CompletedDashboard = true, ProjectionDashboard = true, TimeToFill1Dashboard = true, TimeToFIll2Dashboard = true, SkillsDashboard = true, ProgressDashboard = true, User = user21 };
             var preference21 = new Preference { CasualtiesDashboard = true, ProcessesDashboard = true, CompletedDashboard = true, ProjectionDashboard = true, TimeToFill1Dashboard = true, TimeToFIll2Dashboard = true, SkillsDashboard = true, ProgressDashboard = true, User = user22 };
-            
+            var preference22 = new Preference { CasualtiesDashboard = true, ProcessesDashboard = true, CompletedDashboard = true, ProjectionDashboard = true, TimeToFill1Dashboard = true, TimeToFIll2Dashboard = true, SkillsDashboard = true, ProgressDashboard = true, User = user23 };
+
             context.Preferences.Add(preference1);
             context.Preferences.Add(preference2);
             context.Preferences.Add(preference3);
@@ -722,6 +725,7 @@ namespace Domain.Services.Repositories.EF
             context.Preferences.Add(preference19);
             context.Preferences.Add(preference20);
             context.Preferences.Add(preference21);
+            context.Preferences.Add(preference22);
 
             context.SaveChanges();
             #endregion
