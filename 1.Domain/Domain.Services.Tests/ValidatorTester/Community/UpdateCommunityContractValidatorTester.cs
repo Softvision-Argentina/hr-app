cf6f5872f4c962446c0b0d0aa2ff55027e011041
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-12 09:55:51 -0300

    Added unit testing for fluent validations / UpdateCommunityContractValidator

---
+++ 1.Domain/Domain.Services.Tests/ValidatorTester/Community/UpdateCommunityContractValidatorTester.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Services.Impl.Validators.Community;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Domain.Services.Tests.ValidatorTester.Community
+{
+    public class UpdateCommunityContractValidatorTester
+    {
+        private UpdateCommunityContractValidator _validator;
+        public UpdateCommunityContractValidatorTester()
+        {
+            _validator = new UpdateCommunityContractValidator();
+        }
+
+        [Fact(DisplayName = "Verify that throws error when Name string is null")]
+        public void Should_Have_Error_When_Name_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(CommunityContract => CommunityContract.Name, (string) null);
+        }
+
+        [Fact(DisplayName = "Verify that throws error when Name string is empty")]
+        public void Should_Have_Error_When_Name_Is_Empty()
+        {
+            _validator.ShouldHaveValidationErrorFor(CommunityContract => CommunityContract.Name, String.Empty);
+        }
+
+        [Fact(DisplayName = "Verify that throws error when Description string is null")]
+        public void Should_Have_Error_When_Description_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(CommunityContract => CommunityContract.Description, (string) null);
+        }
+
+        [Fact(DisplayName = "Verify that throws error when Description string is Empty")]
+        public void Should_Have_Error_When_Description_Is_Empty()
+        {
+            _validator.ShouldHaveValidationErrorFor(CommunityContract => CommunityContract.Description, String.Empty);
+        }
+
+
+
+    }
+}
