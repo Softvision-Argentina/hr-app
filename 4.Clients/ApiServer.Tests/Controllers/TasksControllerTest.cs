b18575ddf0b16cc88f67a5b022967bfe3d5f1ceb
Henry Montana <henry.montana@softvision.com>
2020-03-16 15:22:50 -0300

    # Conflicts: #	4.Clients/ApiServer.Tests/Controllers/TasksControllerTest.cs

--- 4.Clients/ApiServer.Tests/Controllers/TasksControllerTest.cs
+++ 4.Clients/ApiServer.Tests/Controllers/TasksControllerTest.cs
@@ -32,10 +32,10 @@ namespace ApiServer.Tests.Controllers
             mockTaskService.Setup(_ => _.List()).Returns(new List<ReadedTaskContract>());
             mockMapper.Setup(_ => _.Map<List<ReadedTaskViewModel>>(It.IsAny<ReadedTaskContract>())).Returns(new List<ReadedTaskViewModel>());
 
-            var actionResult = controller.Get();
+            var result = controller.Get();
 
-            Assert.NotNull(actionResult);
-            Assert.IsType<AcceptedResult>(actionResult);
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
             mockTaskService.Verify(_ => _.List(), Times.Once);
             mockMapper.Verify(_ => _.Map<List<ReadedTaskViewModel>>(It.IsAny<List<ReadedTaskContract>>()), Times.Once);
         }
@@ -45,11 +45,11 @@ namespace ApiServer.Tests.Controllers
         {
             int id = 0;
 
-            var actionResult = controller.Get(id);
+            var result = controller.Get(id);
 
-            Assert.NotNull(actionResult);
-            Assert.IsType<NotFoundObjectResult>(actionResult);
-            Assert.Equal(id, (actionResult as NotFoundObjectResult).Value);
+            Assert.NotNull(result);
+            Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal(id, (result as NotFoundObjectResult).Value);
             mockTaskService.Verify(_ => _.Read(It.IsAny<int>()), Times.Once);
         }
 
@@ -59,12 +59,96 @@ namespace ApiServer.Tests.Controllers
             int id = 0;
             mockTaskService.Setup(_ => _.Read(It.IsAny<int>())).Returns(new ReadedTaskContract());
 
-            var actionResult = controller.Get(id);
+            var result = controller.Get(id);
 
-            Assert.NotNull(actionResult);
-            Assert.IsType<AcceptedResult>(actionResult);
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
             mockTaskService.Verify(_ => _.Read(It.IsAny<int>()), Times.Once);
             mockMapper.Verify(_ => _.Map<ReadedTaskViewModel>(It.IsAny<ReadedTaskContract>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Verify that post with CreateTaskViewModel returns CreatedResult when data is valid")]
+        public void Should_PostCreateTaskViewModel_When_DataIsValid()
+        {
+            CreateTaskViewModel task = new CreateTaskViewModel();
+            mockTaskService.Setup(_ => _.Create(It.IsAny<CreateTaskContract>())).Returns(new CreatedTaskContract());
+
+            var result = controller.Post(task);
+
+            Assert.NotNull(result);
+            Assert.IsType<CreatedResult>(result);
+            Assert.Equal("Get", (result as CreatedResult).Location);
+            mockTaskService.Verify(_ => _.Create(It.IsAny<CreateTaskContract>()), Times.Once);
+            mockMapper.Verify(_ => _.Map<CreateTaskContract>(It.IsAny<CreateTaskViewModel>()), Times.Once);
+            mockMapper.Verify(_ => _.Map<CreatedTaskViewModel>(It.IsAny<CreatedTaskContract>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Verify that put with id and UpdateTaskViewModel returns AcceptedResult when data is valid")]
+        public void Should_PutAcceptedResult_When_DataIsValid()
+        {
+            int id = 0;
+            UpdateTaskViewModel task = new UpdateTaskViewModel();
+            mockMapper.Setup(_ => _.Map<UpdateTaskContract>(It.IsAny<UpdateTaskViewModel>())).Returns(new UpdateTaskContract());
+
+            var result = controller.Put(id, task);
+
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
+            Assert.Equal(new { id }.ToQueryString(), (result as AcceptedResult).Value.ToQueryString());
+            mockTaskService.Verify(_ => _.Update(It.IsAny<UpdateTaskContract>()), Times.Once);
+            mockMapper.Verify(_ => _.Map<UpdateTaskContract>(It.IsAny<UpdateTaskViewModel>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Verify that delete with id returns AcceptedResult when data is valid")]
+        public void Should_DeleteAcceptedResult_When_DataIsValid()
+        {
+            int id = 0;
+
+            var result = controller.Delete(id);
+
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
+            mockTaskService.Verify(_ => _.Delete(It.IsAny<int>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Verify that ping returns OkObjectResult")]
+        public void Should_PingOkObjectResult()
+        {
+            var result = controller.Ping();
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(new { Status = "OK" }.ToQueryString(), (result as OkObjectResult).Value.ToQueryString());
+        }
+
+        [Fact(DisplayName = "Verify that aprroves returns AcceptedResult when data is valid")]
+        public void Should_ApproveAcceptedResult_When_DataIsValid()
+        {
+            int id = 0;
+            
+            var result = controller.Approve(id);
+
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
+            Assert.Equal(new { id }.ToQueryString(), (result as AcceptedResult).Value.ToQueryString());
+            mockTaskService.Verify(_ => _.Approve(It.IsAny<int>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Verify that GetByConsultant returns AcceptedResult when data is valid")]
+        public void Should_GetByConsultant_When_DataIsValid()
+        {
+            string consultantEmail = null;
+            var expectedValue = new List<ReadedTaskViewModel>();
+            mockTaskService.Setup(_ => _.ListByConsultant(It.IsAny<string>())).Returns(new List<ReadedTaskContract>());
+            mockMapper.Setup(_ => _.Map<List<ReadedTaskViewModel>>(It.IsAny<IEnumerable<ReadedTaskContract>>())).Returns(expectedValue);
+
+            var result = controller.GetByConsultant(consultantEmail);
+
+            Assert.NotNull(result);
+            Assert.IsType<AcceptedResult>(result);
+            Assert.Equal(expectedValue, (result as AcceptedResult).Value);
+            mockTaskService.Verify(_ => _.ListByConsultant(It.IsAny<string>()), Times.Once);
+            mockMapper.Verify(_ => _.Map<List<ReadedTaskViewModel>>(It.IsAny<IEnumerable<ReadedTaskContract>>()), Times.Once);
         }
     }
 }
