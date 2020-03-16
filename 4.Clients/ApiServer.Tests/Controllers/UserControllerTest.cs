941c4ddbf3f4aa8a4aaf3c3b310bf32aebe12b03
Henry Montana <henry.montana@softvision.com>
2020-03-16 17:48:34 -0300

    Add UserControllerTest

---
+++ 4.Clients/ApiServer.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using ApiServer.Controllers;
+using AutoMapper;
+using Core;
+using Domain.Services.Interfaces.Services;
+using Moq;
+
+namespace ApiServer.Tests.Controllers
+{
+    public class UserControllerTest
+    {
+        private readonly UserController controller;
+        private readonly Mock<IUserService> mockTaskService;
+        private readonly Mock<ILog<UserController>> mockLog;
+        private readonly Mock<IMapper> mockMapper;
+
+        public UserControllerTest()
+        {
+            mockTaskService = new Mock<IUserService>();
+            mockLog = new Mock<ILog<UserController>>();
+            mockMapper = new Mock<IMapper>();
+            controller = new UserController(mockTaskService.Object, mockLog.Object, mockMapper.Object);
+        }
+    }
+}
