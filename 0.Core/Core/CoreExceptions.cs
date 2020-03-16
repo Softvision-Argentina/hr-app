0616605d251a50ec383d0d74b75a0abfa9b1c9a5
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-16 10:16:08 -0300

    Created a new exception to handle blank, null, and white spaced string in AuditableExtensions

--- 0.Core/Core/CoreExceptions.cs
+++ 0.Core/Core/CoreExceptions.cs
@@ -7,10 +7,17 @@ namespace Core
     public class CoreExceptionConstants
     {
         public static readonly string InvalidSettingMessage = "Invalid setting; {0}.";
+        public static readonly string InvalidAuditableUserName = "Argument cannot be null, empty or white spaces";
     }
 
     public class InvalidSettingException : Exception
     {
         public InvalidSettingException(string message) : base(message) { }
     }
+
+    public class InvalidAuditableUsernameException : Exception
+    {
+        public InvalidAuditableUsernameException() : base(CoreExceptionConstants.InvalidAuditableUserName) { }
+    }
+
 }
