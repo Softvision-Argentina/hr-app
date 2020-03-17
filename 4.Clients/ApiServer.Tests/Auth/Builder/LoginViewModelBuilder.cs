5f048f3ddba27a9fef0ee60bc1beb7750eb794f4
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-17 11:42:26 -0300

    Added data generetor for loginviewmodel class

---
+++ 4.Clients/ApiServer.Tests/Auth/Builder/LoginViewModelBuilder.cs
@@ -0,0 +1,35 @@
+﻿using ApiServer.Contracts.Login;
+using System;
+
+namespace ApiServer.Tests.Candidates.Builder
+{
+    public class LoginViewModelBuilder
+    {
+        private string Username { get; set; }
+        private string Password { get; set; }
+
+        public LoginViewModelBuilder()
+        {
+            Username = $"{Guid.NewGuid()}";
+            Password = $"{Username}#password";
+        }
+
+        public LoginViewModel GetInvalidData()
+        {
+            return new LoginViewModel()
+            {
+                UserName = this.Username,
+                Password = this.Password
+            };
+        }
+
+        public LoginViewModel GetValidData()
+        {
+            return new LoginViewModel()
+            {
+                UserName = "nicolas.roldan@softvision.com",
+                Password = "1234"
+            };
+        }
+    }
+}
