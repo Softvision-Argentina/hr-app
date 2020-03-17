eec89081fd1f4376a535c02418b3202db28075a9
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-17 11:41:47 -0300

    Add utility class for http related tasks

---
+++ 0.Core/Core/HttpUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+
+namespace Core
+{
+    public class HttpVerb
+    {
+        public const string GET = "GET";
+        public const string POST = "POST";
+        public const string DELETE = "DELETE";
+        public const string PUT = "PUT";
+    }
+
+    public class HttpResultData
+    {
+        public HttpResponseMessage response { get; set; }
+        public string responseString { get; set; }
+    }
+}
