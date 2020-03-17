f709f62c70ae550c6079ee84c20905b2fd49bd21
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-17 11:44:30 -0300

    Created a generic method that handles httpverbs "HttpCall" for a more easy implementation of functional test

--- 4.Clients/ApiServer.Tests/Seed/BaseApiTest.cs
+++ 4.Clients/ApiServer.Tests/Seed/BaseApiTest.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Core;
 
 namespace ApiServer.Tests.Seed
 {
@@ -35,6 +37,90 @@ namespace ApiServer.Tests.Seed
             AssertSuccess(response, responseString);
 
             return JsonConvert.DeserializeObject<U>(responseString);
+        }
+
+        private async Task<HttpResultData> GetHttpCall<T>(string controllerName, T model, string controllerMethod = "", string param = "")
+        {
+            var _response = await Client.GetAsync($"/api/{controllerName}/{controllerMethod}/{param}", HttpCompletionOption.ResponseContentRead);
+            var _responseString = await _response.Content.ReadAsStringAsync();
+
+            var result = new HttpResultData
+            {
+                response = _response,
+                responseString = _responseString
+            };
+
+            return result;
+        }
+
+        private async Task<HttpResultData> DeleteHttpCall<T>(string controllerName, T model, string controllerMethod = "", string param = "")
+        {
+            var _response = await Client.DeleteAsync($"/api/{controllerName}/{controllerMethod}/{param}");
+            var _responseString = await _response.Content.ReadAsStringAsync();
+
+            var result = new HttpResultData
+            {
+                response = _response,
+                responseString = _responseString
+            };
+
+            return result;
+        }
+
+        private async Task<HttpResultData> PostHttpCall<T>(string controllerName, T model, string controllerMethod = "", string param = "")
+        {
+            var _response = await Client.PostAsync($"/api/{controllerName}/{controllerMethod}/{param}",
+                new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json"));
+
+            var _responseString = await _response.Content.ReadAsStringAsync();
+
+            var result = new HttpResultData
+            {
+                response = _response,
+                responseString = _responseString
+            };
+
+            return result;
+        }
+
+        private async Task<HttpResultData> PutHttpCall<T>(string controllerName, T model, string controllerMethod = "")
+        {
+            var _response = await Client.PutAsync($"/api/{controllerName}/{controllerMethod}",
+                new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json"));
+
+            var _responseString = await _response.Content.ReadAsStringAsync();
+
+            var result = new HttpResultData
+            {
+                response = _response,
+                responseString = _responseString
+            };
+
+            return result;
+        }
+
+        protected async Task<HttpResultData> HttpCall<T>(string httpVerb, T model, string controllerName, string controllerMethod = "", string param = "") where T: class
+        {
+            HttpResultData result;
+
+            switch (httpVerb)
+            {
+                case HttpVerb.GET:
+                    result = await GetHttpCall(controllerName, model, controllerMethod);
+                    break;
+                case HttpVerb.POST:
+                    result = await PostHttpCall(controllerName, model, controllerMethod);
+                    break;
+                case HttpVerb.PUT:
+                    result = await PutHttpCall(controllerName, model, controllerMethod);
+                    break;
+                case HttpVerb.DELETE:
+                    result = await DeleteHttpCall(controllerName, model, controllerMethod);
+                    break;
+                default: throw new NotImplementedException("Http verb is not implemented");
+            }
+
+            return result;
         }
     }
 }
