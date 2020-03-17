5827a203ddde606ff67cf53994e6fa98d4a3ab8a
Henry Montana <henry.montana@softvision.com>
2020-03-17 17:54:22 -0300

    - Add UserServiceTest
    - Remove UserService's DataBaseContext dependency and clean code

--- 1.Domain/Domain.Services.Impl/Services/UserService.cs
+++ 1.Domain/Domain.Services.Impl/Services/UserService.cs
@@ -19,21 +19,18 @@ namespace Domain.Services.Impl.Services
         private readonly IRepository<User> _userRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly ILog<UserService> _log;
-        private readonly DataBaseContext _dbcontext;
 
         public UserService(IMapper mapper, IRepository<User> userRepository,
-                           IUnitOfWork unitOfWork, ILog<UserService> log, DataBaseContext dbContext)
+                           IUnitOfWork unitOfWork, ILog<UserService> log)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
             _userRepository = userRepository;
             _log = log;
-            _dbcontext = dbContext;
         }
 
         public ReadedUserContract Authenticate(string username, string password)
         {
-            #region V1
             var user = Login(username, password);
 
             if (user != null)
@@ -44,10 +41,6 @@ namespace Domain.Services.Impl.Services
             {
                 return null;
             }
-            #endregion
-
-            #region V2
-            #endregion
         }
 
         public ReadedUserContract Authenticate(string username)
@@ -88,7 +81,7 @@ namespace Domain.Services.Impl.Services
 
         private User Login(string username, string password)
         {
-            var user = _dbcontext.Users.FirstOrDefault(x => x.Username == username && x.Password == HashUtility.GetStringSha256Hash(password));
+            var user = _userRepository.Query().FirstOrDefault(x => x.Username == username && x.Password == HashUtility.GetStringSha256Hash(password));
 
             if (user == null)
                 return null;
@@ -98,7 +91,7 @@ namespace Domain.Services.Impl.Services
 
         private User ExternalLogin(string username)
         {
-            var user = _dbcontext.Users.FirstOrDefault(x => x.Username == username);
+            var user = _userRepository.Query().FirstOrDefault(x => x.Username == username);
 
             if (user == null)
                 return null;
