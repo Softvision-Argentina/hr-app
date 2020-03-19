a60d038a9ec4cf793b9419c599c3010f123c1cf6
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-19 11:45:20 -0300

    Base class to create a new integration test. Has a Client, Server, and could extract instances of services though Service.GetService method

---
+++ Domain.IntegrationTests/Services/BaseServiceIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Core;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Domain.Services.IntegrationTests.Services
+{
+    [Collection("Service collection")]
+    public class BaseServiceIntegrationTest
+    {
+        protected HttpClient Client { get; }
+        protected TestServer Server { get; }
+        protected IServiceProvider Services { get;  }
+
+        public BaseServiceIntegrationTest(ServiceFixture serviceFixture)
+        {
+            Client = serviceFixture.Client;
+            Server = serviceFixture.Server;
+            Services = serviceFixture.Server.Host.Services;
+        }
+    }
+}
