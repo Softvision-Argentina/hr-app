6e2e6ca40ce9e20985755d456e71fcff5067e684
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-19 11:44:00 -0300

    Created a shareable context fixture (xunit) to avoid duplicate code in later integration tests

---
+++ Domain.IntegrationTests/ServiceFixture.cs
@@ -0,0 +1,63 @@
+using ApiServer;
+using Domain.Services.Repositories.EF;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Domain.Services.IntegrationTests
+{
+    public class ServiceFixture : IDisposable
+    {
+        private static readonly object Sync = new object();
+        private static bool _configured;
+        private static string _env = "IntegrationTest";
+
+        public ServiceFixture()
+        {
+            lock (Sync)
+            {
+                if (!_configured)
+                {
+                    var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{_env}.json", optional: false, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+
+                    Server = new TestServer(WebHost.CreateDefaultBuilder()
+                        .UseEnvironment(_env)
+                        .UseConfiguration(builder.Build())
+                        .UseStartup<Startup>());
+
+                    Services = Server.Host.Services;
+
+                    Client = Server.CreateClient();
+
+                    _configured = true;
+                }
+            }
+        }
+
+        public TestServer Server { get; internal set; }
+        public HttpClient Client { get; internal set; }
+        public IServiceProvider Services { get; internal set; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Server.Dispose();
+        }
+    }
+
+    [CollectionDefinition("Service collection")]
+    public class ServiceCollection : ICollectionFixture<ServiceFixture>
+    {
+
+    }
+}
