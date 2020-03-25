a2dbd05f4509205047cc66cb460da59bf38b0d22
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-25 18:04:40 -0300

    Create helper methods to clean the data of the database for each integration test, and also created methods to seed the DB. Added it to persistance EF layer to be avaible to all type of tests

---
+++ 2.Infrastructure/Persistance.EF/Extensions/ContextExtension.cs
@@ -0,0 +1,71 @@
+﻿using Core;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ApiServer.Contracts;
+
+namespace Persistance.EF.Extensions
+{   
+    public static class ContextExtension
+    {
+        public static string DisableTablesConstraintCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' ";
+        public static string DeleteDatabaseDataCommand = "EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'";
+        public static string EnableTablesConstraintCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'";
+    }
+
+    public static class TestDatabaseHelper
+    {
+        public static IEnumerable<Entity<int>> EntitiesSeeded;
+        public static Entity<int> EntitySeeded;
+
+        public static void SetupDatabaseForTesting(this DbContext context)
+        {
+            EntitiesSeeded = null;
+            EntitySeeded = null;
+            context.Database.EnsureCreated();
+            context.Database.ExecuteSqlCommand(ContextExtension.DisableTablesConstraintCommand);
+            context.Database.ExecuteSqlCommand(ContextExtension.DeleteDatabaseDataCommand);
+            context.Database.ExecuteSqlCommand(ContextExtension.EnableTablesConstraintCommand);
+        }
+
+        public static void SeedDatabaseWith<T>(this DbContextBase arLabContext, IEnumerable<T> entities) where T : Entity<int>
+        {
+            List<T> entityList = new List<T>();
+
+            foreach (var entity in entities)
+            {
+                entity.WithPropertyValue("Id", default(int));
+                entityList.Add(entity as T);
+            }
+
+            arLabContext.Set<T>().AddRange(entityList);
+            arLabContext.SaveChanges();
+            EntitiesSeeded = entityList;
+        }
+
+        public static void SeedDatabaseWith<T>(this DbContextBase arLabContext, T entity) where T : Entity<int>
+        {
+            entity.WithPropertyValue("Id", default(int));
+            arLabContext.Set<T>().Add(entity);
+            arLabContext.SaveChanges();
+            EntitySeeded = entity;
+        }
+
+        public static void SeedDatabaseWithDummy<T>(this DbContextBase arLabContext, int entityCount = 1) where T : Entity<int>
+        {
+            List<T> entityList = new List<T>();
+
+            for (int i = 0; i < entityCount; i++)
+            {
+                var dummyEntity = DataFactory.CreateInstance<T>()
+                    .WithPropertyValue("Id", default(int));
+
+                entityList.Add(dummyEntity as T);
+            }
+
+            arLabContext.Set<T>().AddRange(entityList);
+            arLabContext.SaveChanges();
+            EntitiesSeeded = entityList;
+        }
+    }
+}
