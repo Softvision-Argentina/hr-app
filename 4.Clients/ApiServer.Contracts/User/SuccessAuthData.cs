189343fbaa57498878b731e1711966b010c2e740
Rodrigo Ramírez <rodrigo.ramirez@softvision.com>
2020-03-27 18:19:21 -0300

    Added SuccessAuthData to store the json response to a success external login

---
+++ 4.Clients/ApiServer.Contracts/User/SuccessAuthData.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiServer.Contracts.User
+{
+    public class SuccessAuthData
+    {
+        public string Token { get; set; }
+        public ReadedUserViewModel User { get; set; }
+    }
+}
