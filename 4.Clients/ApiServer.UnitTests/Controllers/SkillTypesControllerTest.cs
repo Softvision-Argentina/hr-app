86663c0b4b171e2eef9b15e2364062e446ec44ad
Henry Montana <henry.montana@softvision.com>
2020-03-30 11:52:55 -0300

    Move SkillsControllerTest and SkillTypesControllerTest to ApiServer.UnitTests

--- 4.Clients/ApiServer.UnitTests/Controllers/SkillTypesControllerTest.cs
+++ 4.Clients/ApiServer.UnitTests/Controllers/SkillTypesControllerTest.cs
@@ -3,13 +3,14 @@ using ApiServer.Contracts.SkillType;
 using ApiServer.Controllers;
 using AutoMapper;
 using Core;
+using Core.ExtensionHelpers;
 using Domain.Services.Contracts.SkillType;
 using Domain.Services.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
 
-namespace ApiServer.Tests.Controllers
+namespace ApiServer.UnitTests.Controllers
 {
     public class SkillTypesControllerTest
     {
