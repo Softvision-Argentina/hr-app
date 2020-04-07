eebd710777cd716f92e1d59b5fc37f9046bf5382
Henry Montana <henry.montana@softvision.com>
2020-04-07 16:07:18 -0300

    Add SkillTypeRepositoryTest, StageItemRepositoryTest, TaskItemRepositoryTest, TaskRepositoryTest and TechnicalStageRepositoryTest

---
+++ 2.Infrastructure/Domain.Services.Repositories.EF.UnitTests/SkillTypeRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Domain.Services.Repositories.EF.UnitTests
+{
+    public class SkillTypeRepositoryTest : BaseRepositoryTest
+    {
+        private readonly SkillTypeRepository _repository;
+
+        public SkillTypeRepositoryTest()
+        {
+            _repository = new SkillTypeRepository(DbContext, MockUnitOfWork.Object);
+        }
+
+        [Fact(DisplayName = "Verify that repository returns null when Query there is no data")]
+        public void GivenNotDataInRepositorysDbcontext_WhenQuery_ThenReturnsNull()
+        {
+            SkillType expectedValue = null;
+
+            var actualValue = _repository.Query();
+
+            Assert.NotNull(actualValue);
+            Assert.Equal(0, actualValue.Count());
+            Assert.Equal(expectedValue, actualValue.FirstOrDefault());
+        }
+
+        [Fact(DisplayName = "Verify that repository returns task when Query there is data")]
+        public void GivenDataInRepositorysDbcontext_WhenQuery_ThenReturnsSkillType()
+        {
+            var expectedValue = new SkillType();
+            DbContext.SkillTypes.Add(expectedValue);
+            DbContext.SaveChanges();
+
+            var actualValue = _repository.Query();
+
+            Assert.NotNull(actualValue);
+            Assert.Equal(1, actualValue.Count());
+            Assert.Equal(expectedValue, actualValue.FirstOrDefault());
+        }
+
+        [Fact(DisplayName = "Verify that repository returns task when QueryEager there is data")]
+        public void GivenDataInRepositorysDbcontext_WhenQueryEager_ThenReturnsSkillTypes()
+        {
+            var expectedValue = new SkillType()
+            {
+                Skills= new List<Skill>() { new Skill() }
+            };
+            DbContext.SkillTypes.Add(expectedValue);
+            DbContext.SaveChanges();
+
+            var actualValue = _repository.QueryEager();
+
+            Assert.NotNull(actualValue);
+            Assert.Equal(1, actualValue.Count());
+            Assert.Equal(expectedValue, actualValue.FirstOrDefault());
+            Assert.Equal(expectedValue.Skills.Count, actualValue.FirstOrDefault().Skills.Count);
+            Assert.Equal(expectedValue.Skills.FirstOrDefault(), actualValue.FirstOrDefault().Skills.FirstOrDefault());
+        }
+    }
+}
---
+++ 2.Infrastructure/Domain.Services.Repositories.EF.UnitTests/StageItemRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Domain.Services.Repositories.EF.UnitTests
+{
+    public class StageItemRepositoryTest : BaseRepositoryTest
+    {
+        private readonly StageItemRepository _repository;
+
+        public StageItemRepositoryTest()
+        {
+            _repository = new StageItemRepository(DbContext, MockUnitOfWork.Object);
+        }
+
+        [Fact(DisplayName = "Verify that repository returns null when Query there is no data")]
+        public void GivenNotDataInRepositorysDbcontext_WhenQuery_ThenReturnsNull()
+        {
+            StageItem expectedValue = null;
+
+            var actualValue = _repository.Query();
+
+            Assert.NotNull(actualValue);
+            Assert.Equal(0, actualValue.Count());
+            Assert.Equal(expectedValue, actualValue.FirstOrDefault());
+        }
+
+        [Fact(DisplayName = "Verify that repository returns StageItem when Query there is data")]
+        public void GivenDataInRepositorysDbcontext_WhenQuery_ThenReturnsStageItem()
+        {
+            var expectedValue = new StageItem();
+            DbContext.StageItems.Add(expectedValue);
+            DbContext.SaveChanges();
+
+            var actualValue = _repository.Query();
+
+            Assert.NotNull(actualValue);
+            Assert.Equal(1, actualValue.Count());
+            Assert.Equal(expectedValue, actualValue.FirstOrDefault());
+        }
+    }
+}
