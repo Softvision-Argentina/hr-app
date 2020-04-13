f1027a8485832b6f28a614f73ceb0e4c42614cff
Rodrigo Ramirez <rodrigo.ramirez@softvision.com>
2020-04-13 12:40:06 -0300

    naming convention minor changes

--- 2.Infrastructure/Persistance.EF/Extensions/ContextExtension.cs
+++ 2.Infrastructure/Persistance.EF/Extensions/ContextExtension.cs
@@ -6,10 +6,10 @@ namespace Persistance.EF.Extensions
 {
     public static class ContextExtension
     {
-        public readonly static string DisableTablesConstraintsCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' ";
-        public readonly static string DeleteDatabaseDataCommand = "EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'";
-        public readonly static string EnableTablesConstraintsCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'";
-        public readonly static string ResetAllIds = "EXEC sp_MSForEachTable 'IF OBJECTPROPERTY(object_id(''?''), ''TableHasIdentity'') = 1 DBCC CHECKIDENT(''?'', RESEED, 0)'";
+        public static readonly string DisableTablesConstraintsCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' ";
+        public static readonly string DeleteDatabaseDataCommand = "EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'";
+        public static readonly string EnableTablesConstraintsCommand = "EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'";
+        public static readonly string ResetAllIds = "EXEC sp_MSForEachTable 'IF OBJECTPROPERTY(object_id(''?''), ''TableHasIdentity'') = 1 DBCC CHECKIDENT(''?'', RESEED, 0)'";
     }
 
     public static class TestDatabaseHelper
@@ -33,31 +33,31 @@ namespace Persistance.EF.Extensions
             context.Database.ExecuteSqlCommand(ContextExtension.EnableTablesConstraintsCommand);
         }
 
-        public static void SeedDatabaseWith<T>(this DbContextBase arLabContext, IEnumerable<T> entities) where T : Entity<int>
+        public static void SeedDatabaseWith<T>(this DbContextBase context, IEnumerable<T> entities) where T : Entity<int>
         {
             var entityList = new List<T>();
 
             foreach (var entity in entities)
             {
                 entity.WithPropertyValue("Id", default(int));
-                entityList.Add(entity as T);
+                entityList.Add((T) entity);
             }
 
-            arLabContext.Set<T>().AddRange(entityList);
-            arLabContext.SaveChanges();
+            context.Set<T>().AddRange(entityList);
+            context.SaveChanges();
             EntitiesSeeded = entityList;
         }
 
-        public static void SeedDatabaseWith<T>(this DbContextBase arLabContext, T entity) where T : Entity<int>
+        public static void SeedDatabaseWith<T>(this DbContextBase context, T entity) where T : Entity<int>
         {
             entity.WithPropertyValue("Id", default(int));
-            arLabContext.Set<T>().Add(entity);
-            arLabContext.SaveChanges();
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
             EntitySeeded = entity;
-            arLabContext.Entry(entity).State = EntityState.Detached;
+            context.Entry(entity).State = EntityState.Detached;
         }
 
-        public static void SeedDatabaseWithDummy<T>(this DbContextBase arLabContext, int entityCount = 1) where T : Entity<int>
+        public static void SeedDatabaseWithDummy<T>(this DbContextBase context, int entityCount = 1) where T : Entity<int>
         {
             var entityList = new List<T>();
 
@@ -66,11 +66,11 @@ namespace Persistance.EF.Extensions
                 var dummyEntity = DataFactory.CreateInstance<T>()
                     .WithPropertyValue("Id", default(int));
 
-                entityList.Add(dummyEntity as T);
+                entityList.Add((T) dummyEntity);
             }
 
-            arLabContext.Set<T>().AddRange(entityList);
-            arLabContext.SaveChanges();
+            context.Set<T>().AddRange(entityList);
+            context.SaveChanges();
             EntitiesSeeded = entityList;
         }
     }
