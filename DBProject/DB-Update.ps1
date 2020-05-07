f265a32d8f9f12b78a1209aeacd149d6e1000e41
rodrigo.ramirez <rodrigo.ramirez@softvision.com>
2020-05-07 19:46:31 +0000

    Merged PR 68: US34/TASK84: Added Functional test environment to db-update script
    Related work items: #34, #84

--- DBProject/DB-Update.ps1
+++ DBProject/DB-Update.ps1
@@ -4,19 +4,20 @@ param(
     $Migration
 )
 
-Write-Host "Begin: updating databases" `n -ForegroundColor cyan
+Write-Host "Begin: Updating databases" `n -ForegroundColor cyan
 
 $ProjectName = "ApiServer"
 $DevelopmentEnvironment = "Development"
 $IntegrationEnvironment = "IntegrationTest"
-$EnvArray = $DevelopmentEnvironment, $IntegrationEnvironment
+$FunctionalEnvironment = "FunctionalTest"
+$EnvArray = $DevelopmentEnvironment, $IntegrationEnvironment, $FunctionalEnvironment
 
 foreach ($Env in $EnvArray){  
     $env:ASPNETCORE_ENVIRONMENT = $Env
     Write-Host "ASPNETCORE_ENVIRONMENT is now: "$Env `n -ForegroundColor cyan
-    Write-Host "Updating database with environment : " $Env `n -ForegroundColor cyan            
+    Write-Host "Updating database with environment: " $Env `n -ForegroundColor cyan            
     update-database $Migration
 }
 
 $env:ASPNETCORE_ENVIRONMENT = ""
-Write-Host "End: Updated databases, ASPNETCORE_ENVIRONMENT is $env:ASPNETCORE_ENVIRONMENT" `n -ForegroundColor green
+Write-Host "End: Updatin databases" `n -ForegroundColor green
