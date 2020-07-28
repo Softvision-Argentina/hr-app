2db2aca3067e03c079e627ac99720a5c4663cebb
Rodrigo Ramirez <rodrigo.ramirez@softvision.com>
2020-07-28 18:50:01 -0300

    changes

--- scripts/release-git-history-writter.ps1
+++ scripts/release-git-history-writter.ps1
@@ -1,6 +1,7 @@
 cd "_recru-db\Sql"
 git status
-git checkout $(Build.SourceBranchName)
+echo !$(Build.SourceBranchName)
+git checkout develop
 git add .
 git commit -m "Save changes for build in last commit: $(Build.SourceVersion)"
 git push
