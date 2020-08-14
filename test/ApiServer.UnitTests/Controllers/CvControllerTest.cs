82937ea664f63cffbf9c9c9cd6a0b80196f64e82
ivan zerzer <ivan.zerzer@softvision.com>
2020-08-14 13:46:37 -0300

    fix

--- test/ApiServer.UnitTests/Controllers/CvControllerTest.cs
+++ test/ApiServer.UnitTests/Controllers/CvControllerTest.cs
@@ -27,24 +27,5 @@
             this.mockAzure = new Mock<IAzureUploadService>();
             this.controller = new CvController(this.mockCandidateService.Object, this.mockAzure.Object, this.mockCvService.Object);
         }
-
-        [Fact(DisplayName = "Verify that method 'AddCv' returns OkObjectResult")]
-        public void Should_AddCv()
-        {
-            var candidateId = 0;
-            var cvContract = new CvContractAdd();
-
-            this.mockCandidateService.Setup(_ => _.GetCandidate(It.IsAny<int>())).Returns(new Candidate());
-            this.mockAzure.Setup(_ => _.Upload(It.IsAny<IFormFile>(), It.IsAny<Candidate>())).ReturnsAsync(It.IsAny<string>());
-            this.mockCvService.Setup(_ => _.StoreCvAndCandidateCvId(It.IsAny<Candidate>(), It.IsAny<CvContractAdd>(), It.IsAny<string>()));
-
-            var result = this.controller.AddCv(candidateId, cvContract);
-
-            Assert.NotNull(result);
-            Assert.IsType<OkObjectResult>(result);
-            this.mockCandidateService.Verify(_ => _.GetCandidate(It.IsAny<int>()), Times.Once);
-            this.mockAzure.Verify(_ => _.Upload(It.IsAny<IFormFile>(), It.IsAny<Candidate>()), Times.Once);
-            this.mockCvService.Verify(_ => _.StoreCvAndCandidateCvId(It.IsAny<Candidate>(), It.IsAny<CvContractAdd>(), It.IsAny<string>()), Times.Once);
-        }
     }
 }
