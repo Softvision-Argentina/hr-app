f751d47ba801900147620d4fb47d8de3d18f690a
Ivan Zerzer <ivan.zerzer@softvision.com>
2020-08-20 19:20:52 +0000

    Merged PR 452: migration for preoffer checks to allow nulls
    migration for preoffer checks to allow nulls
    !451
    
    Related work items: #578

---
+++ migrations/TwentyThird.sql
@@ -0,0 +1,25 @@
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[PreOfferStages]') AND [c].[name] = N'PreocupationalDone');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [PreOfferStages] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [PreOfferStages] ALTER COLUMN [PreocupationalDone] bit NULL;
+
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[PreOfferStages]') AND [c].[name] = N'BackgroundCheckDone');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [PreOfferStages] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [PreOfferStages] ALTER COLUMN [BackgroundCheckDone] bit NULL;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200820185609_TwentyThird', N'3.1.0');
+
+GO
+
