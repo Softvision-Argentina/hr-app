b515d21e001089ff923f2f25705092a1e5d9783c
Rodrigo Ramirez <rodrigo.ramirez@softvision.com>
2020-08-21 13:13:34 +0000

    Automatic commit on date: 08/21/2020 13:13:33 | Sent applied migration to history folder for release triggered by build number: 20200821.1

---
+++ history/masters_applied/master-21082020-13-13.sql
@@ -0,0 +1,53 @@
+--Start: 2020-08-21 13:00:33Z
+BEGIN TRY
+    BEGIN TRANSACTION
+
+----Start:TwentyThird.sql / migration
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200820185609_TwentyThird')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[PreOfferStages]') AND [c].[name] = N'PreocupationalDone');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [PreOfferStages] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [PreOfferStages] ALTER COLUMN [PreocupationalDone] bit NULL;
+END;
+
+
+
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200820185609_TwentyThird')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[PreOfferStages]') AND [c].[name] = N'BackgroundCheckDone');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [PreOfferStages] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [PreOfferStages] ALTER COLUMN [BackgroundCheckDone] bit NULL;
+END;
+
+
+
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200820185609_TwentyThird')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200820185609_TwentyThird', N'3.1.0');
+END;
+
+
+
+
+
+----End:TwentyThird.sql / migration
+
+    COMMIT
+ 
+END TRY
+BEGIN CATCH
+    ROLLBACK
+END CATCH
+--End: 2020-08-21 13:00:33Z
